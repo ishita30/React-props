4af03fdf45e77e825b46a0f720d87bde14a16776
ishita30 <ishitasinghal30@gmail.com>
2022-01-15 19:02:00 +0530

    Added folder

--- mapping-components/src/components/App.jsx
+++ mapping-components/src/components/App.jsx
@@ -3,13 +3,22 @@ import Card from './Card'
 import contacts from '../contacts'
 
 function createCard(contact) {
-  return <Card name={contact.name} />
+  return (
+    <Card
+      id={contact.id}
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  )
 }
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Avatar />
       {contacts.map(createCard)}
 
       {/* <Card
