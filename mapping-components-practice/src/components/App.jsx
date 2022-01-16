a8262ce4e6f9b3f0640bf5678f32423c8c8b7a85
ishita30 <ishitasinghal30@gmail.com>
2022-01-16 23:59:49 +0530

    Updated files

--- mapping-components-practice/src/components/App.jsx
+++ mapping-components-practice/src/components/App.jsx
@@ -4,6 +4,17 @@ import emojipedia from '././emojipedia'
 
 console.log(emojipedia)
 
+function createEntry(emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      description={emojiTerm.meaning}
+    />
+  )
+}
+
 function App() {
   return (
     <div>
